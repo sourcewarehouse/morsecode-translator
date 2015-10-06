3304840c80d833f6d3f9d5d71e128f2253a0c25c
Malaz Alamir <malaz.alamir@hotmail.com>
2015-10-05 21:48:39 -0400

    Finished the translator.

--- src/MorseCodeTranslator.java
+++ src/MorseCodeTranslator.java
@@ -2,53 +2,79 @@ import java.util.Scanner;
 
 public class MorseCodeTranslator {
 	static Scanner input = new Scanner(System.in);
-	
-	String userInput;
+
 	static String seq = "";
-	static String[][] EnglishAndMorse = { {"a", ".-"}, {"b", "-..."}, {"c", "-.-."}, {"d", "-.."}, {"e", "."},
-								  {"f", "..-."}, {"g", "--."}, {"h", "...."}, {"i", ".."}, {"j", ".---"},
-								  {"k", "-.-"}, {"l", ".-.."}, {"m", "--"}, {"n", "-."}, {"o", "---"},
-								  {"p", ".--."}, {"q","--.-" }, {"r", ".-."}, {"s", "..."}, {"t", "-"},
-								  {"u", "..-"}, {"v", "...-"}, {"w", ".--"}, {"x", "-..-"}, {"y", "-.--"},
-								  {"z", "--.."}, {" ", "  "}};
 
 	static String[] English = {"a", "b", "c", "d","e","f", "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "};
 	static String[] Morse = {".-", "-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","  "};
-	
+
 	public static void main(String[] args) {
-		System.out.println("Enter your phrase, english or morse, I understand both mawfucka: ");
-		String phrase = input.nextLine();
-		phrase.toLowerCase();
-		translate(phrase, Morse, English);
-		
+		boolean playAgain = true;
+		String anotherOne = "";
+		do {
+			System.out.println("Enter your phrase, english or morse, I understand both: ");
+			String phrase = input.nextLine();
+			phrase.toLowerCase();
+			if (isEnglish(phrase)==1) {
+				translateToMorse(phrase, English, Morse);
+			} else if (isEnglish(phrase)==2) {
+				translateToEnglish(phrase, Morse, English);
+			} else {
+				System.out.println("Please enter Morse or English, not a combination. Not sure what you meant buddeh.");
+			}
+
+			do {
+				System.out.println();
+				System.out.println("Another one? yes/no (please only enter yes or no)");
+
+				anotherOne = input.nextLine();
+			} while (!anotherOne.equalsIgnoreCase("yes") && !anotherOne.equalsIgnoreCase("no"));
+			if (anotherOne.equalsIgnoreCase("yes")) playAgain = true;
+			if (anotherOne.equalsIgnoreCase("no")) playAgain = false; System.out.println("BYE!");
+		} while(playAgain);
+
+
 	}
-	
-	public static void translate(String phrase, String[] original, String[] translated) {
+
+	// translate english to morse
+	public static void translateToMorse(String phrase, String[] English, String[] Morse) {
 		String seq = "";
 		for (int i=0; i<phrase.length(); i++) {
-			for (int j=0; j<original.length; j++) {
-				if (original[j].equals(phrase.charAt(i) +"")) {
-					seq += translated[j];
-	
+			for (int j=0; j<English.length; j++) {
+				if (English[j].equals(phrase.charAt(i) +"")) {
+					seq += Morse[j];
+				}
+			}
+			System.out.print(seq);
+			seq = "";
+		}	
+	}
+
+	//translate morse to english
+	public static void translateToEnglish(String phrase, String[] Morse, String[] English) {
+		String seq = "";
+		String[] result = phrase.split(" ");
+		for (int i=0; i<result.length; i++) {
+			for (int j=0; j<Morse.length; j++) {
+				if (Morse[j].equals(result[i])) {
+					seq += English[j];
 				}
 			}
 			System.out.print(seq + " ");
 			seq = "";
-		}	
+		}
+	}
+
+	//check if statement is english or more
+	public static int isEnglish(String phrase) {
+		int checkType = 0;
+			if (phrase != null && phrase.length() > 0 && phrase.charAt(0) > 0x60 && phrase.charAt(0) < 0x7b) {
+				checkType = 1;
+			} else if (phrase.contains("-") && phrase.contains(".") && (phrase.charAt(0)=='-' || phrase.charAt(0)=='.') || phrase.charAt(0)==' ') {
+				checkType = 2;
+			} else {
+				checkType = 0;
+			}
+		return checkType;
 	}
 }
-		
-//		for (int i=0; i<phrase.length(); i++) {
-//				for (int j=0; j<EnglishAndMorse.length; j++) {
-//					if (EnglishAndMorse[j][0].equals(phrase.charAt(i) + "")) {
-//						seq += EnglishAndMorse[j][1];
-//					}
-//					
-//					
-//				}
-//				System.out.print(seq + " ");
-//				seq = "";
-//			}
-
-
-
